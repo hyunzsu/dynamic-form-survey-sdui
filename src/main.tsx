ba398fb39a38fbda1e6b26d882f60d14456e67dd
hyunzsu <hyunzsu@naver.com>
2025-12-14 00:46:52 +0900

    feat: 절대 경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App, Providers } from '@/app';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Providers>
+      <App />
+    </Providers>
+  </StrictMode>
+);
